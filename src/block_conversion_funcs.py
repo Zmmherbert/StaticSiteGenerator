9f899406b6a44d1350ff14654451b19b17df82f3
zmmherbert <zmmherbert@gmail.com>
2025-07-21 00:53:37 -0400

    completed block_to_block_type function

--- src/block_conversion_funcs.py
+++ src/block_conversion_funcs.py
@@ -1,4 +1,47 @@
+from enum import Enum
+import re
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
     return list(filter(lambda item: item != "", list(map(lambda item: item.strip(), markdown.split("\n\n")))))
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} .*", block):
+        return BlockType.HEADING
+    elif re.match(r"^```[\s\S]*```$", block):
+        return BlockType.CODE
+    elif re.match(r"^(>.*\n*)+$", block):
+        return BlockType.QUOTE
+    elif re.match(r"^(- .*\n*)+$", block):
+        return BlockType.UNORDERED_LIST
+    elif re.match(r"^(\d\. .*\n*)+$", block) and all(list(map(lambda item: int(item[1][0]) == item[0] + 1, enumerate(block.split("\n"))))):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+    
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.HEADING:
+                pass
+            case BlockType.CODE:
+                pass
+            case BlockType.QUOTE:
+                pass
+            case BlockType.UNORDERED_LIST:
+                pass
+            case BlockType.ORDERED_LIST:
+                pass
+            case BlockType.PARAGRAPH:
+                pass
+            case _:
+                pass
--- src/test_block_funcs.py
+++ src/test_block_funcs.py
@@ -37,5 +37,19 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_type(self):
+        block_type_heading = block_to_block_type('### This is a heading')
+        self.assertEqual(block_type_heading, BlockType.HEADING)
+        block_type_code = block_to_block_type('```THERES SOME CODE HERE ```')
+        self.assertEqual(block_type_code, BlockType.CODE)
+        block_type_quote = block_to_block_type('>QUOTE 1\n> QUOTE 2')
+        self.assertEqual(block_type_quote, BlockType.QUOTE)
+        block_type_unordered_list = block_to_block_type('- item\n- another item\n- and another')
+        self.assertEqual(block_type_unordered_list, BlockType.UNORDERED_LIST)
+        block_type_ordered_list = block_to_block_type('1. item 1\n2. item 2\n3. item 3')
+        self.assertEqual(block_type_ordered_list, BlockType.ORDERED_LIST)
+        block_type_paragraph = block_to_block_type('Just some words and shit\nidk really')
+        self.assertEqual(block_type_paragraph, BlockType.PARAGRAPH)
+
 if __name__ == "__main__":
     unittest.main()
